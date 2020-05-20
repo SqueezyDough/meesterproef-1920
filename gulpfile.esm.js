9f831439f67db2599818e13176e4f4bbee5f66fa
SqueezyDough <squeezydough@outlook.com>
2020-05-20 12:37:39 +0200

    Add gulp watcher for js files

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,21 +1,19 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
-
-
 import gulp from 'gulp'
-import sass from 'gulp-sass'
-import cleanCSS from 'gulp-clean-css'
 import browserSyncLib from 'browser-sync'
+import gulpLoadPlugins from 'gulp-load-plugins'
+
+const plugins = gulpLoadPlugins(),
+	browserSync = browserSyncLib.create()
+
 
 require("dotenv").config();
 const PORT = process.env.PORT || 8888
 
-const browserSync = browserSyncLib.create()
-
 gulp.task("sass", function() {
 	return gulp.src("./dev-assets/sass/**/*.scss")
-		.pipe(sass())
-		.on("error", sass.logError)
-		.pipe(cleanCSS())
+		.pipe(plugins.sass())
+		.on("error", plugins.sass.logError)
+		.pipe(plugins.cleanCss())
 		.pipe(gulp.dest("static/bundle"))
 		.pipe(browserSync.reload({
 			stream: true
@@ -27,5 +25,19 @@ gulp.task("watch", function() {
 		proxy: `localhost:${PORT}`
 	});
 
-	gulp.watch("./dev-assets/sass/**/*.scss", gulp.series("sass"));
+	gulp.watch("./dev-assets/sass/**/*.scss", gulp.task("sass"));
+	gulp.watch("./dev-assets/js/**/*.js", gulp.task("minify-js"));
 }); 
+
+gulp.task('minify-js', function(done) {
+	gulp.src('dev-assets/js/**/*.js')
+		.pipe(plugins.plumber())
+		.pipe(plugins.concat('main.js'))
+		.pipe(plugins.babel({
+			presets: ['@babel/env']
+		}))
+	   	.pipe(plugins.uglify())
+		.pipe(gulp.dest('static/bundle'))
+		   
+	done()
+ });
