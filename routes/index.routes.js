3c51160dee5a55ba8bbea0e962f4d3f99795e8f1
damian1997 <d.veltkamp137@gmail.com>
2020-05-22 11:13:37 +0200

    Added snapshot functionality to save snapshot of webcamstream to server

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const base = require('../controllers/default.controller')
 const ROUTER = express.Router()
+var multer  = require('multer')
+var upload = multer()
 
 ROUTER.get('/', base.home)
+ROUTER.post('/upload-snapshot', upload.single('file'), base.snapshot)
 
 module.exports = ROUTER
