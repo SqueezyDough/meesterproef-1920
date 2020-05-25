2af18578021895ff7ce32a47aa26b5b3073d2cd1
damian1997 <d.veltkamp137@gmail.com>
2020-05-25 15:16:04 +0200

    First implementation of tesseract detection inside browser

--- dev-assets/js/scanner.js
+++ dev-assets/js/scanner.js
@@ -6,6 +6,11 @@ if (navigator.mediaDevices.getUserMedia) {
 
   switch(window_path_split[1]) {
     case 'scan-medicine':
+      const tesseract_worker = Tesseract.createWorker({
+        //logger: m => console.log(m)
+      })
+      //Tesseract.setLogging(true)
+
       navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
@@ -16,9 +21,7 @@ if (navigator.mediaDevices.getUserMedia) {
       })
         .then((stream) => {
           video.srcObject = stream
-          setInterval(() => {
-            createSnapshot(video)
-          }, 2000)
+          recognizeRVG(tesseract_worker)
         })
         .catch((err) => {
           console.log('Error: ', err)
@@ -29,7 +32,7 @@ if (navigator.mediaDevices.getUserMedia) {
   console.log('Your browser down not support video streams')
 }
 
-function createSnapshot(video) {
+function getBase64Image() {
   const canvas = document.createElement('canvas'),
     context = canvas.getContext('2d')
 
@@ -37,23 +40,42 @@ function createSnapshot(video) {
   canvas.height = video.offsetHeight
   context.drawImage(video, 0 ,0, video.offsetWidth, video.offsetHeight)
 
-  const image_base64 = canvas.toDataURL('image/jpeg')
-
-  const form_data = new FormData()
-  form_data.append('file', image_base64)
-
-  fetch('/upload-snapshot', {
-    method: 'POST',
-    body: form_data
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Display ts output
-    appendTesseractOutput(data)
-  })
+  return canvas.toDataURL('image/jpeg')
 }
 
 function appendTesseractOutput(output) {
   const tesseract_output_container = document.getElementById('tesseract_output_container')
   tesseract_output_container.innerHTML = `Scanned text: ${output}`
+}
+
+// TODO EXIT STATEMENT SHOULD BE WHEN MEDICINE HAS BEEN RETRIEVED FROM API, AND WORKER.TERMINATE
+async function recognizeRVG(tesseract_worker) {
+  let result = undefined
+  const base64_image= getBase64Image()
+  await tesseract_worker.load()
+  await tesseract_worker.loadLanguage('eng')
+  await tesseract_worker.initialize('eng')
+  
+  try {
+    result = await tesseract_worker.recognize(base64_image)
+    
+    if(result.data.text === '') throw 'No words detected'
+
+    const rvg_index = result.data.words.findIndex(word => {
+      return word.text.toLowerCase() === 'rvg'
+    })
+    
+    // Check if RVG has been found inside word list
+    if(rvg_index === -1) throw 'RVG not detected inside words list'
+    
+    // Check if RVG code is of correct format
+    if(/^\d+$/.test(result.data.words[rvg_index+1].text) !== true) throw 'Suspected code is not of correct format'
+    
+    console.log(result.data.words[rvg_index+1])
+    console.log(result.data.words)
+  }
+  catch(error) {
+    console.log(error)
+    recognizeRVG(tesseract_worker)
+  }
 }
