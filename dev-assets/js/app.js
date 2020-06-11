22e9976bff00a86b13ecc29c8eadcf90384abb4b
SqueezyDough <squeezydough@outlook.com>
2020-06-11 17:06:25 +0200

    Fix body width

--- dev-assets/js/app.js
+++ dev-assets/js/app.js
@@ -1,33 +1,12 @@
 const details_checkboxes = document.querySelectorAll('.card__details-checkbox')
 
-const fix_scroll = () => {
-  const scroll_y = document.documentElement.style.getPropertyValue('--scroll-y')
-  const body = document.body
-
-  body.style.position = 'fixed'
-  body.style.top = `-${scroll_y}`
-}
-
-const enable_scroll = () => {
-  const body = document.body
-  const scroll_y = body.style.top
-  body.style.position = ''
-  body.style.top = ''
-  window.scrollTo(0, parseInt(scroll_y || '0') * -1)
-}
-
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty('--scroll-y', `${window.scroll_y}px`)
-})
-
 // fix scroll when modal is open
 details_checkboxes.forEach(chk => {
   chk.addEventListener('click', () => {
-    // source: https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
     if (chk.checked) {
-      fix_scroll()
+      document.body.style.position = 'fixed'
     } else {
-      enable_scroll()
+      document.body.style.position = ''
     }  
   })
 })
